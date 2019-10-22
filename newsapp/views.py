0609e7f7cc65ea32cee704a854d565ed5b5c1385
Mikolaj Buchwald <mikbuch@users.noreply.github.com>
2019-10-22 08:33:48 +0200

    BBC News

--- newsapp/views.py
+++ newsapp/views.py
@@ -12,7 +12,7 @@ def index(request):
 
     newsapi = NewsApiClient(api_key='22126ca24dde4a71b63dfa2db3210ea5')
 
-    top = newsapi.get_top_headlines(sources='techcrunch')
+    top = newsapi.get_top_headlines(sources='bbc-news')
 
     # Pass the latest articles as list of dictionaries to index template.
     # Some of the keys in the dictionary are: 'title', 'urlToImage' and
